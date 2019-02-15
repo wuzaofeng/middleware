2d1ffce923b13cf291b75486f8156f0fb91e7cb3
wuzaofeng <673908452@qq.com>
2019-02-15 10:57:38 +0800

    修改可查询user列表

--- controller/blog/user.js
+++ controller/blog/user.js
@@ -6,31 +6,41 @@ const barIconModel = require('../../schema/blog/barIcon')
 
 const user = {
   read: async (ctx, next) => {
-    const { _id, authorSrc, username, location, email } = await userModel.findOne({ email: '673908452@qq.com' })
-    const tags = await tagsModel.find()
-    const articles = await articlesModel.find()
-    const categories = await categoriesModel.find()
-    const barIcon = await barIconModel.find()
-
-    ctx.body = {
-      _id, authorSrc, username, location, email,
-      count: {
-        tags: tags.length,
-        articles: articles.length
-      },
-      tags: tags.map(i => ({
-        name: i.name,
-        type: i.type
-      })),
-      categories: categories.map(i => ({
-        name: i.name,
-        count: i.count
-      })),
-      barIcon: barIcon.map(i => ({
-        link: i.link,
-        icon: i.icon,
-        type: i.type
-      }))
+    const { email } = ctx.request.query
+    // 通过email来查询单个用户
+    if (email) {
+      const { _id, authorSrc, username, location } = await userModel.findOne({ email: '673908452@qq.com' })
+      const tags = await tagsModel.find()
+      const articles = await articlesModel.find()
+      const categories = await categoriesModel.find()
+      const barIcon = await barIconModel.find()
+  
+      ctx.body = {
+        _id, authorSrc, username, location, email,
+        count: {
+          tags: tags.length,
+          articles: articles.length
+        },
+        tags: tags.map(i => ({
+          name: i.name,
+          type: i.type
+        })),
+        categories: categories.map(i => ({
+          name: i.name,
+          count: i.count
+        })),
+        barIcon: barIcon.map(i => ({
+          link: i.link,
+          icon: i.icon,
+          type: i.type
+        }))
+      }
+    } else {
+      const list = await userModel.find()
+      ctx.body = {
+        list,
+        total: list.length
+      }
     }
   },
   create: async (ctx, next) => {
